4fc9ba8a0cff2126b79ed879694f443986885777
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2026-03-02 11:36:25 +0200

    Update ExampleInstrumentedTest.kt

--- sample/src/androidTest/java/com/mushafimad/sampleapp/ExampleInstrumentedTest.kt
+++ sample/src/androidTest/java/com/mushafimad/sampleapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mushafimad.sampleapp", appContext.packageName)
+        assertEquals("com.mushafimad.sampleapp.debug", appContext.packageName)
     }
 }
